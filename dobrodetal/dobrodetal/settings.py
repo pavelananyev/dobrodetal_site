83674d110fe693df78d4eca33d8655e29e2ab319
pavel <ananyev.pawell@gmail.com>
2023-04-10 06:17:04 +0300

    urls and redirect

--- dobrodetal/dobrodetal/settings.py
+++ dobrodetal/dobrodetal/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-from SC import SCd
+from dobrodetal.SK import SKey
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,12 +20,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = SCd
+SECRET_KEY = SKey
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'landing.apps.LandingConfig'
 ]
 
 MIDDLEWARE = [
---
+++ dobrodetal/landing/views.py
@@ -0,0 +1,29 @@
+from django.shortcuts import render, redirect
+from django.http import HttpResponse, HttpResponseNotFound
+
+
+def getout(request):
+    out = ''
+    if request.GET:
+        getdict = request.GET
+        for key, val in getdict.items():
+            out += f'<p>Ключ: {key}; значение: {val}</p>'
+        print(request.GET)
+        print(out)
+    return out
+
+
+# Create your views here.
+def main(request):
+    gout = getout(request)
+    return HttpResponse(f'Поставка запчастей для сельхозтехники' + bool(gout) * f'<p>GET запрос:</p><p>{gout}</p>')
+
+
+def contact(request):
+    gout = getout(request)
+    return HttpResponse(f'Контакты' + bool(gout) * f'<p>GET запрос:</p><p>{gout}</p>')
+
+
+def pageNotFound(request, exception):
+#    return HttpResponseNotFound('<h1>Страница не найдена</h1>')
+    return redirect('home', permanent=True)
