f36faea1a1b8c6dd73546f171ea4a6cd19dd0a48
pavel <ananyev.pawell@gmail.com>
2023-04-30 22:50:53 +0300

    add first db configuration

--- dobrodetal/landing/models.py
+++ dobrodetal/landing/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+def photos_directory(instance, filename):
+    return f'/photos/{instance.article}/{filename}'
+class Detail(models.Model):
+    title = models.CharField(max_length=255)
+    article = models.CharField(max_length=255, blank=True)
+    photo = models.ImageField(upload_to=photos_directory)
+    description = models.TextField()
+    price = models.PositiveIntegerField()
+    second_price = models.PositiveIntegerField(blank=True)
+    available = models.BooleanField(default=True)
+    quantity = models.PositiveSmallIntegerField(default=0)
+    time_create = models.DateTimeField(auto_now_add=True)
+    time_update = models.DateTimeField(auto_now=True)
+    is_published = models.BooleanField(default=True)
